c2dde589507079d0ef1b6e016c41dc71050c5d25
jeremy wiles-young <jeremywiles-young@jbones.local>
2018-12-06 00:20:20 -0500

    better

--- bg-lines.js
+++ bg-lines.js
@@ -1,28 +1,30 @@
 var array1 = []
-var arrayNum 
+var arrayNum, xamt, yamt
 
 function setup() {
-  var myCanvas = createCanvas(1200, 800);
+  var myCanvas = createCanvas(1600, 800);
   myCanvas.parent('myContainer');
-  var arrayNum = 3*(parseInt(width/100) + parseInt(height/100));
+  xamt = parseInt(width/100);
+  yamt = parseInt(height/100);
+  var arrayNum = 3*(xamt+yamt);
   //var array1 = Array(arrayNum);
-  for (var x = 0; x < arrayNum*3; x++) {
+  for (var x = 0; x < arrayNum; x++) {
        array1[x] = parseInt(random(-20,20));
        }
-  background(255);
+  background(255,255,255,0);
   noLoop();
 }
 
 function draw() {
-  background(255);
+  
   
   for (var x = 0; x < 12; x++) {
     for (var y = 0; y < 8; y++) {
-    stroke(234,123,245);
-    strokeWeight(1);
+    stroke(234,123,245,175);
+    strokeWeight(2);
     
-    line((x*100)+(array1[x]*100), (y*100)+(array1[y+1]*100), 
-         (x*100)+(array1[x+2]*200), (y*100)+(array1[y+3]*150))
+    line((x*100)+(array1[x]*40), (y*100)+(array1[y+1]*20), 
+         (x*100)+(array1[x+2]*40), (y*100)+(array1[y+3]*20))
        }
   }
 }
