6ef818a32b5504dd5c296f484b85b49652663325
Jeremy <jwilesyoung@gmail.com>
2018-12-28 13:06:12 -0500

    implement rgb

--- bg-lines.js
+++ bg-lines.js
@@ -23,7 +23,7 @@ function draw() {
   
   for (var x = 0; x < 12; x++) {
     for (var y = 0; y < 8; y++) {
-    stroke(234,123,245,175);
+    stroke(r,g,b,175);
     strokeWeight(2);
     
     line((x*100)+(array1[x]*40), (y*100)+(array1[y+1]*20), 
