3938aef40fde0021fbfa9f67767da6ca8f22ffa9
jbones <jwilesyoung@gmail.com>
2019-01-06 14:24:22 -0500

    hmm

--- bg-lines.js
+++ bg-lines.js
@@ -1,11 +1,14 @@
 var array1 = []
-var arrayNum, xamt, yamt
+var arrayNum, xamt, yamt, r, g, b
 
 function setup() {
   var myCanvas = createCanvas(1600, 800);
   myCanvas.parent('myContainer');
   xamt = parseInt(width/100);
   yamt = parseInt(height/100);
+  r = parseInt(random(50, 250));
+  g = parseInt(random(50, 250));
+  b = parseInt(random(50, 250));
   var arrayNum = 3*(xamt+yamt);
   //var array1 = Array(arrayNum);
   for (var x = 0; x < arrayNum; x++) {
@@ -20,7 +23,7 @@ function draw() {
   
   for (var x = 0; x < 12; x++) {
     for (var y = 0; y < 8; y++) {
-    stroke(234,123,245,175);
+    stroke(r,g,b,175);
     strokeWeight(2);
     
     line((x*100)+(array1[x]*40), (y*100)+(array1[y+1]*20), 
